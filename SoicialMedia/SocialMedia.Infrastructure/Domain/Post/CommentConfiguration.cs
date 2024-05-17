04ead74714bed4cac2f5edcef10d797b82f8aa0d
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-18 00:32:05 +0200

    Created CommentConfiguration

---
+++ SoicialMedia/SocialMedia.Infrastructure/Domain/Post/CommentConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMedia.Domain.Entities;
+
+namespace SocialMedia.Infrastructure.Domain.Post;
+
+public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.ToTable("Comments");
+        builder.HasKey(x => x.Id);
+        
+        //Relationship with Post
+        builder
+            .HasOne(x => x.Post)
+            .WithMany(x => x.Comments)
+            .HasForeignKey(x => x.PostId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+        //Relationship with User
+        builder
+            .HasOne(x => x.User)
+            .WithMany(x => x.Comments)
+            .HasForeignKey(x => x.UserId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
