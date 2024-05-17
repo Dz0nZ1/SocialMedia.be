2642a209b6eb9c59c42da58c72f95e8e313cb1ff
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-17 21:06:35 +0200

    Created Message Entity

---
+++ SoicialMedia/SocialMedia.Domain/Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿using SocialMedia.Domain.Entities.User;
+
+namespace SocialMedia.Domain.Entities;
+
+public class Message
+{
+    public Guid Id { get; set; }
+    public string SenderId { get; set; }
+    public string ReceiverId { get; set; }
+    public string Content { get; set; }
+    public string CreatedAt { get; set; }
+    public string? ReadAt { get; set; } 
+}
