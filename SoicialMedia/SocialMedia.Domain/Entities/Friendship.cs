3f34d09aef120a84b3189a90d019dddb4822cd14
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-17 21:12:04 +0200

    Created Friendship Entity

---
+++ SoicialMedia/SocialMedia.Domain/Entities/Friendship.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialMedia.Domain.Entities;
+
+public class Friendship
+{
+    public Guid Id { get; set; }
+    
+    public string FirstUserId { get; set; }
+    
+    public string SeconedUserId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    
+    public string StatusEnum { get; set; }
+}
