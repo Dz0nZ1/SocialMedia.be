ecd874dacf7f3a32adabc4ed4ccd3f5b65872bcd
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-17 20:59:58 +0200

    Created Event Entity

---
+++ SoicialMedia/SocialMedia.Domain/Entities/Event.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace SocialMedia.Domain.Entities;
+
+public class Event
+{
+    public Guid EventId { get; set; }
+    
+    public string OrganizerId { get; set; }
+    
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+    
+    public string Location { get; set; }
+    
+    public DateTime StartTime { get; set; }
+    
+    public DateTime EndTime { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime ModifiedAt { get; set; }
+}
