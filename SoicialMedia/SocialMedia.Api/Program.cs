b85bdd2f1c86609ee12aed662d22555a43fa3285
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-18 00:35:55 +0200

    Added filters, swagger, auth and cors to program

--- SoicialMedia/SocialMedia.Api/Program.cs
+++ SoicialMedia/SocialMedia.Api/Program.cs
@@ -1,17 +1,94 @@
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using SocialMedia.Api.Cors;
+using SocialMedia.Api.Filters;
 using SocialMedia.Application;
 using SocialMedia.Infrastructure;
+using SocialMedia.Infrastructure.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<ApiExceptionFilterAttribute>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = JwtBearerDefaults.AuthenticationScheme,
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter your token below.",
+        });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
+//Application Layers
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
+
+
+//Authentication and Authorization
+var jwtConfiguration = new JwtConfiguration();
+builder.Configuration
+    .GetSection("JwtConfiguration")
+    .Bind(jwtConfiguration);
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidAudience = jwtConfiguration.ValidAudience,
+            ValidIssuer = jwtConfiguration.ValidIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfiguration.Secret!))
+        };
+    });
+
+
+//Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(WebApplicationConstants.NEXTJS_APPLICATION, policyBuilder =>
+    {
+        policyBuilder.WithOrigins(WebApplicationConstants.NEXTJS_APPLICATION_BASE_URL);
+        policyBuilder.AllowAnyHeader();
+        policyBuilder.AllowAnyMethod();
+        policyBuilder.AllowCredentials();
+    });
+});
+
+
 
 var app = builder.Build();
 
@@ -21,6 +98,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(WebApplicationConstants.NEXTJS_APPLICATION);
 
 app.UseHttpsRedirection();
 
