4c5b689fae9483092a28e25633eb3ed27ef4e0d1
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-17 21:05:41 +0200

    Created Like Entity

---
+++ SoicialMedia/SocialMedia.Domain/Entities/Like.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialMedia.Domain.Entities;
+
+public class Like
+{
+    public Guid Id { get; set; }
+
+    public string UserId { get; set; }
+
+    public Guid? PostId { get; set; }
+
+    public Guid? CommentId { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+}
