a3d5875aa46e9965010801bdaf0e25f31b2f0845
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-18 00:34:47 +0200

    Added mappings for EF

--- SoicialMedia/SocialMedia.Application/Common/interfaces/ISmDbContext.cs
+++ SoicialMedia/SocialMedia.Application/Common/interfaces/ISmDbContext.cs
@@ -1,7 +1,17 @@
-﻿namespace SocialMedia.Application.Common.interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Domain.Entities;
+using SocialMedia.Domain.Entities.User;
+
+namespace SocialMedia.Application.Common.interfaces;
 
 public interface ISmDbContext
 {
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
-    // public DbSet<Domain.Entities.Event> Events { get; }
+    public DbSet<Comment> Comments { get; }
+    public DbSet<Event> Events { get; }
+    public DbSet<Friendship> Friendships { get; }
+    public DbSet<Like> Likes { get; }
+    public DbSet<Message> Messages { get; }
+    public DbSet<Post> Posts { get; }
+    public DbSet<ApplicationUser> Users { get; }
 }
