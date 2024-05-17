5e319975d69470b98e0becca8306f1de9c800f2a
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-17 20:59:04 +0200

    Created Comment Entity

---
+++ SoicialMedia/SocialMedia.Domain/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿namespace SocialMedia.Domain.Entities;
+
+public class Comment
+{
+    public Guid Id { get; set; }
+    
+    public Guid PostId { get; set; }
+    
+    public string UserId { get; set; }
+    
+    public string Content { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime ModifiedAt { get; set; }
+    
+    public int LikeCount { get; set; }
+}
