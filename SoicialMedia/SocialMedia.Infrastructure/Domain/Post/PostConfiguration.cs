7bc957f29236f2b61f98b8e4a77ef2702f7437d4
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-18 00:33:23 +0200

    Created PostConfiguration

---
+++ SoicialMedia/SocialMedia.Infrastructure/Domain/Post/PostConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SocialMedia.Infrastructure.Domain.Post;
+
+public class PostConfiguration : IEntityTypeConfiguration<SocialMedia.Domain.Entities.Post>
+{
+    public void Configure(EntityTypeBuilder<SocialMedia.Domain.Entities.Post> builder)
+    {
+        builder.ToTable("Posts");
+        builder.HasKey(x => x.Id);
+
+        //Relation with User
+        builder
+            .HasOne(x => x.User)
+            .WithMany(x => x.Posts)
+            .HasForeignKey(x => x.UserId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+        
+    }
+}
