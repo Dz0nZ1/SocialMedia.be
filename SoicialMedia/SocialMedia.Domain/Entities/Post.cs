c79a7a34990d7a977fc474003e26fb8e5cbe88a2
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-17 21:10:49 +0200

    Created Post Entity

---
+++ SoicialMedia/SocialMedia.Domain/Entities/Post.cs
@@ -0,0 +1,24 @@
+﻿using SocialMedia.Domain.Entities.User;
+
+namespace SocialMedia.Domain.Entities;
+
+public class Post
+{
+    public Guid Id { get; private set; }
+    
+    public string UserId { get;  private set; }
+    
+    public ApplicationUser User { get;  private set; }
+    
+    public string Content { get;  private set; }
+    
+    public string ImageUrl { get;  private set; }
+    
+    public DateTime CreatedAt { get;  private set; }
+    
+    public DateTime ModifiedAt { get; private set; }
+    
+    public int LikeCount { get; private set; }
+    
+    public int CommentCount { get; private set; }
+}
