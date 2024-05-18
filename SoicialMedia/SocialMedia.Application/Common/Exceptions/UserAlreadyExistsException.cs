d2e6a7149b8d1ab7a516c3bab21d9ddbdd45d0be
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-18 16:19:25 +0200

    Created UserAlreadyExistsException

---
+++ SoicialMedia/SocialMedia.Application/Common/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialMedia.Application.Common.Exceptions;
+
+public class UserAlreadyExistsException : BaseException
+{
+    public UserAlreadyExistsException(string message, object? additionalData = null) : base(message, additionalData)
+    {
+    }
+
+    public UserAlreadyExistsException(string message, Exception innerException, object? additionalData = null) : base(message, innerException, additionalData)
+    {
+    }
+}
