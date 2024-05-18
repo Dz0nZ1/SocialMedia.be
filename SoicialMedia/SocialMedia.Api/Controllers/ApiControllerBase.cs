ad6f2159b878ee62df61a02c65c7475993622760
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-18 14:52:24 +0200

    Created ApiControllerBase

---
+++ SoicialMedia/SocialMedia.Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMedia.Api.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]/[action]")]
+public class ApiControllerBase : ControllerBase
+{
+    private ISender? _mediator;
+
+    protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+}
