5fa36a08d664b3ffff3e6ec629916d576f591cd1
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:25:05 +0200

    Created GetUserDetailsQuery

---
+++ SoicialMedia/SocialMedia.Application/User/Queries/GetUserDetailsQuery/GetUserDetailsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.User;
+
+namespace SocialMedia.Application.User.Queries.GetUserDetailsQuery;
+
+public record GetUserDetailsQuery(string Id) : IRequest<UserDetailsDto?> ;
---
+++ SoicialMedia/SocialMedia.Application/User/Queries/GetUserDetailsQuery/GetUserDetailsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.User;
+using SocialMedia.Application.Common.Exceptions;
+using SocialMedia.Application.Common.interfaces;
+using SocialMedia.Application.Common.Mappers.User;
+using SocialMedia.Domain.Common.Extensions;
+
+namespace SocialMedia.Application.User.Queries.GetUserDetailsQuery;
+
+public class GetUserDetailsQueryHandler(IUserService userService) : IRequestHandler<GetUserDetailsQuery, UserDetailsDto?>
+{
+    public async Task<UserDetailsDto?> Handle(GetUserDetailsQuery request, CancellationToken cancellationToken)
+    {
+        return await userService.GetUserAsync(request.Id);
+    }
+}
