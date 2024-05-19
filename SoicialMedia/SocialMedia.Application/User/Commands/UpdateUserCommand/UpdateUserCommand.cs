eb914bfd7eaae65343a60685e2f9dd6176b5e131
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:14:01 +0200

    Created UpdateUserCommand

---
+++ SoicialMedia/SocialMedia.Application/User/Commands/UpdateUserCommand/UpdateUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.User;
+
+namespace SocialMedia.Application.User.Commands.UpdateUserCommand;
+
+public record UpdateUserCommand(UpdateUserDetailsDto User) : IRequest<UserDetailsDto?>;
---
+++ SoicialMedia/SocialMedia.Application/User/Commands/UpdateUserCommand/UpdateUserCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.GuardClauses;
+using MediatR;
+using SocialMedia.Application.Common.Dto.User;
+using SocialMedia.Application.Common.interfaces;
+using SocialMedia.Application.Common.Mappers.User;
+using NotFoundException = SocialMedia.Application.Common.Exceptions.NotFoundException;
+
+namespace SocialMedia.Application.User.Commands.UpdateUserCommand;
+
+public class UpdateUserCommandHandler(IUserService userService) : IRequestHandler<UpdateUserCommand, UserDetailsDto?>
+{
+    public async Task<UserDetailsDto?> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        return await userService.UpdateUserAsync(request.User);
+    }
+}
