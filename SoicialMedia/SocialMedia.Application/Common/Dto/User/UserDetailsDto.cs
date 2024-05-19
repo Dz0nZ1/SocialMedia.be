9a93ff4385f735ca01b1172bf1db996665594c2b
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:08:10 +0200

    Created UserDetailsDto

---
+++ SoicialMedia/SocialMedia.Application/Common/Dto/User/UserDetailsDto.cs
@@ -0,0 +1,5 @@
+﻿using SocialMedia.Application.Common.Dto.Post;
+
+namespace SocialMedia.Application.Common.Dto.User;
+
+public record UserDetailsDto(string FirstName, string LastName, string Username, string ProfilePictureUrl, string Bio, List<PostDetailsDto> Posts);
