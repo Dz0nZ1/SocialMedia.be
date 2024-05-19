88d5509abeca88cb4f53f8e523a562a04ffd5822
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:12:52 +0200

    Created UpdateUserDetailsDto

---
+++ SoicialMedia/SocialMedia.Application/Common/Dto/User/UpdateUserDetailsDto.cs
@@ -0,0 +1,3 @@
+﻿namespace SocialMedia.Application.Common.Dto.User;
+
+public record UpdateUserDetailsDto(string UserId, string FirstName, string LastName, string Username, string ProfilePictureUrl, string Bio);
