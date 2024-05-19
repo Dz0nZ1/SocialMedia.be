0ae6b2f0b1c502e94d8580d8b0170becb8d1b3d7
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:11:25 +0200

    Created DeleteUserCommand

---
+++ SoicialMedia/SocialMedia.Application/User/Commands/DeleteUserCommand/DeleteUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Common;
+
+namespace SocialMedia.Application.User.Commands.DeleteUserCommand;
+
+public record DeleteUserCommand(string Id) : IRequest<DeleteResponseDto>;
---
+++ SoicialMedia/SocialMedia.Application/User/Commands/DeleteUserCommand/DeleteUserCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using Ardalis.GuardClauses;
+using MediatR;
+using SocialMedia.Application.Common.Dto.Common;
+using SocialMedia.Application.Common.interfaces;
+using NotFoundException = SocialMedia.Application.Common.Exceptions.NotFoundException;
+
+namespace SocialMedia.Application.User.Commands.DeleteUserCommand;
+
+public class DeleteUserCommandHandler(IUserService userService) : IRequestHandler<DeleteUserCommand, DeleteResponseDto>
+{
+    public async Task<DeleteResponseDto> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        return await userService.DeleteUserAsync(request.Id);
+    }
+}
