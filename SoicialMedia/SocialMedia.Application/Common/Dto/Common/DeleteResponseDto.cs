7666e64cc2ae0dc9c8e9fe09dee4955b33420c57
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:05:19 +0200

    Created DeleteResponseDto

---
+++ SoicialMedia/SocialMedia.Application/Common/Dto/Common/DeleteResponseDto.cs
@@ -0,0 +1,5 @@
+﻿using System.Net;
+
+namespace SocialMedia.Application.Common.Dto.Common;
+
+public record DeleteResponseDto(bool Success, HttpStatusCode StatusCode, string Message, DateTime TimeStamp);
