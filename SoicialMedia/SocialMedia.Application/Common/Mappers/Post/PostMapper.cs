d46c445765847853c19543610d049a066d9cebab
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:21:16 +0200

    Created PostMapper

---
+++ SoicialMedia/SocialMedia.Application/Common/Mappers/Post/PostMapper.cs
@@ -0,0 +1,15 @@
+﻿using Riok.Mapperly.Abstractions;
+using SocialMedia.Application.Common.Dto.Post;
+using SocialMedia.Application.Common.Dto.User;
+
+namespace SocialMedia.Application.Common.Mappers.Post;
+
+[Mapper]
+public static partial class PostMapper
+{
+    public static partial Domain.Entities.Post ToEntity(this CreatePostDetailsDto dto);
+
+    public static partial PostDetailsDto ToDetailsDto(this Domain.Entities.Post entity);
+    
+    public static partial List<PostDetailsDto> ToDetailsListDto(this List<Domain.Entities.Post> entity);
+}
