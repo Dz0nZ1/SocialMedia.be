ed27373d0f43f4960120e045c49d98e72e296bf6
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:19:55 +0200

    Created PostService

---
+++ SoicialMedia/SocialMedia.Application/Common/interfaces/IPostService.cs
@@ -0,0 +1,8 @@
+﻿using SocialMedia.Application.Common.Dto.Post;
+
+namespace SocialMedia.Application.Common.interfaces;
+
+public interface IPostService
+{
+    Task<PostDetailsDto> CreateAsync(CreatePostDetailsDto post, CancellationToken cancellationToken);
+}
---
+++ SoicialMedia/SocialMedia.Infrastructure/Services/PostService.cs
@@ -0,0 +1,21 @@
+﻿using SocialMedia.Application.Common.Dto.Post;
+using SocialMedia.Application.Common.Exceptions;
+using SocialMedia.Application.Common.interfaces;
+using SocialMedia.Application.Common.Mappers.Post;
+
+namespace SocialMedia.Infrastructure.Services;
+
+public class PostService(ISmDbContext dbContext, IUserService userService) : IPostService
+{
+    public async Task<PostDetailsDto> CreateAsync(CreatePostDetailsDto post, CancellationToken cancellationToken)
+    {
+        var user = await userService.GetUserEntityAsync(post.UserId) ?? throw new NotFoundException("Post not found");
+        
+        var entity = post.ToEntity().AddUser(user).AddCreatedAt(DateTime.Now).AddModifiedAt(DateTime.Now);
+        
+        await dbContext.Posts.AddAsync(entity, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken: cancellationToken);
+
+        return entity.ToDetailsDto();
+    }
+}
