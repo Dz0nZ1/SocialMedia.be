2ff244407e553f60c305863fb06afd2ba2e136c1
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:00:18 +0200

    Created AuthController

---
+++ SoicialMedia/SocialMedia.Api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Application.Auth.Commands.BeginLoginCommand;
+using SocialMedia.Application.Auth.Commands.CompleteLoginCommand;
+using SocialMedia.Application.Common.Dto.User;
+using SocialMedia.Application.User.Commands.RegisterUserCommand;
+
+namespace SocialMedia.Api.Controllers;
+
+public class AuthController : ApiControllerBase
+{
+    [AllowAnonymous]
+    [HttpPost]
+    public async Task<ActionResult> BeginLogin(BeginLoginCommand command) => Ok(await Mediator.Send(command));
+
+    [AllowAnonymous]
+    [HttpGet("{validationToken}/CompleteLogin")]
+    public async Task<ActionResult> CompleteLogin([FromRoute] string validationToken) => Ok(await Mediator.Send(new CompleteLoginCommand(validationToken)));
+
+    [AllowAnonymous]
+    [HttpPost]
+    public async Task<ActionResult<UserDetailsDto>> Register(RegisterUserCommand command) =>
+        Ok(await Mediator.Send(command));
+}
