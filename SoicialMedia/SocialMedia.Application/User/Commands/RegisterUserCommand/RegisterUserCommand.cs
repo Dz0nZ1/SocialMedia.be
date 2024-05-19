0970d5ab72c3ef62949d48999e9b353db65fa596
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-19 11:18:45 +0200

    Created RegisterUserCommand

---
+++ SoicialMedia/SocialMedia.Application/User/Commands/RegisterUserCommand/RegisterUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.User;
+
+namespace SocialMedia.Application.User.Commands.RegisterUserCommand;
+
+public record RegisterUserCommand(CreateUserDetailsDto User) : IRequest<UserDetailsDto?>;
---
+++ SoicialMedia/SocialMedia.Application/User/Commands/RegisterUserCommand/RegisterUserCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Constants;
+using SocialMedia.Application.Common.Dto.User;
+using SocialMedia.Application.Common.interfaces;
+using SocialMedia.Application.Common.Mappers.User;
+
+namespace SocialMedia.Application.User.Commands.RegisterUserCommand;
+
+public class RegisterUserCommandHandler(IUserService userService) : IRequestHandler<RegisterUserCommand, UserDetailsDto?>
+{
+    public async Task<UserDetailsDto?> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+      return await userService.CreateUserAsync(request.User,
+           [AuthorizationConstants.User]);
+    }
+}
