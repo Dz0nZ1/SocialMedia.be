ed2f6169917bc513ffdfdf70d22269048c7cf252
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-20 22:55:42 +0200

    Created RefreshToken Entity

---
+++ SoicialMedia/SocialMedia.Domain/Entities/User/RefreshToken.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialMedia.Domain.Entities.User;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; }
+    public string UserId { get; set; }
+
+    public ApplicationUser User;
+    public string Token { get; set; }
+    public DateTime Expiration { get; set; }
+    public bool IsRevoked { get; set; }
+}
