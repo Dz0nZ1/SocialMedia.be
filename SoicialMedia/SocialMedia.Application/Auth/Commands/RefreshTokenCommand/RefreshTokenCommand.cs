d5b5d22e4591e564b3964245c5dcdd23698bce86
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-21 21:41:41 +0200

    Created RefreshTokenCommand

---
+++ SoicialMedia/SocialMedia.Application/Auth/Commands/RefreshTokenCommand/RefreshTokenCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Auth;
+
+namespace SocialMedia.Application.Auth.Commands.RefreshTokenCommand;
+
+public record RefreshTokenCommand(TokenRequest Request) : IRequest<TokenResponse>;
---
+++ SoicialMedia/SocialMedia.Application/Auth/Commands/RefreshTokenCommand/TokenRefreshCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Auth;
+using SocialMedia.Application.Common.interfaces;
+
+namespace SocialMedia.Application.Auth.Commands.RefreshTokenCommand;
+
+public class TokenRefreshCommandHandler(IAuthService authService) : IRequestHandler<RefreshTokenCommand, TokenResponse>
+{
+    public async Task<TokenResponse> Handle(RefreshTokenCommand request, CancellationToken cancellationToken)
+    {
+        return await authService.RefreshTokenAsync(request.Request);
+    }
+}
