9affc26a03a3e0eb8b0cc278036a7a50f6d65b87
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-22 18:44:54 +0200

    Created UserMapper

---
+++ SoicialMedia/SocialMedia.Application/Common/Mappers/User/UserMapper.cs
@@ -0,0 +1,24 @@
+﻿using Riok.Mapperly.Abstractions;
+using SocialMedia.Application.Common.Dto.User;
+using SocialMedia.Application.Common.Mappers.Post;
+using SocialMedia.Domain.Entities.User;
+
+namespace SocialMedia.Application.Common.Mappers.User;
+
+[Mapper]
+public static partial class UserMapper
+{
+    public static UserDetailsDto ToDetailsDto(this ApplicationUser entity)
+    {
+        return new UserDetailsDto(entity.FirstName, entity.LastName, entity.Username, entity.ProfilePictureUrl, entity.Bio, entity.Posts.ToDetailsListDto());
+    }
+    public static partial List<UserDetailsDto> ToDetailsListDto(this List<ApplicationUser> entity);
+    
+    public static void ToEntity(this ApplicationUser entity, UpdateUserDetailsDto dto)
+    {
+        entity.UpdateUser(dto.FirstName, dto.LastName, dto.Username,
+            dto.ProfilePictureUrl, dto.Bio);
+    }
+    
+    
+}
