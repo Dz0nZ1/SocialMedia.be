73f9905cd5700d125ad01aa7422e44157141978d
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-22 19:23:41 +0200

    Created UserController

---
+++ SoicialMedia/SocialMedia.Api/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Application.Common.Constants;
+using SocialMedia.Application.Common.Dto.Common;
+using SocialMedia.Application.Common.Dto.User;
+using SocialMedia.Application.User.Commands.CreateUserCommand;
+using SocialMedia.Application.User.Commands.DeleteUserCommand;
+using SocialMedia.Application.User.Commands.UpdateUserCommand;
+using SocialMedia.Application.User.Queries.GetAllUserDetailsQuery;
+using SocialMedia.Application.User.Queries.GetUserDetailsQuery;
+
+namespace SocialMedia.Api.Controllers;
+
+public class UserController : ApiControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<UserDetailsDto>> GetUserDetails([FromQuery] GetUserDetailsQuery query) =>
+        Ok(await Mediator.Send(query));
+
+    [HttpGet]
+    public async Task<ActionResult<List<UserDetailsDto>>> GetAllUserDetails([FromQuery] GetAllUserDetailsQuery query) =>
+        Ok(await Mediator.Send(query));
+    
+    // [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = AuthorizationConstants.Administrator)]
+    [HttpPost]
+    public async Task<ActionResult> CreateUser(CreateUserCommand command) => Ok(await Mediator.Send(command));
+
+    [HttpPut]
+    public async Task<ActionResult<UserDetailsDto>> UpdateUser(UpdateUserCommand command) =>
+        Ok(await Mediator.Send(command));
+    
+    [HttpDelete]
+    public async Task<ActionResult<DeleteResponseDto>> DeleteUser(DeleteUserCommand command) => Ok(await Mediator.Send(command));
+}
