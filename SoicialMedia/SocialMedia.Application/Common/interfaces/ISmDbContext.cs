6b06e8441f07b861e8b17b787ea234ad7344a5a1
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-22 19:34:39 +0200

    Added references to Entities

--- SoicialMedia/SocialMedia.Application/Common/interfaces/ISmDbContext.cs
+++ SoicialMedia/SocialMedia.Application/Common/interfaces/ISmDbContext.cs
@@ -7,11 +7,11 @@ namespace SocialMedia.Application.Common.interfaces;
 public interface ISmDbContext
 {
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
-    public DbSet<Comment> Comments { get; }
+    public DbSet<Domain.Entities.Comment> Comments { get; }
     public DbSet<Event> Events { get; }
     public DbSet<Friendship> Friendships { get; }
     public DbSet<Like> Likes { get; }
     public DbSet<Message> Messages { get; }
-    public DbSet<Post> Posts { get; }
-    public DbSet<ApplicationUser> Users { get; }
+    public DbSet<Domain.Entities.Post> Posts { get; }
+    public DbSet<RefreshToken> RefreshTokens { get; }
 }
