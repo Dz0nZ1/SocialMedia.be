4a69fabc669f77ac706bf81a7d72d7a4d7d0e8a3
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-22 19:39:55 +0200

    Added Crud methods in PostService

--- SoicialMedia/SocialMedia.Infrastructure/Services/PostService.cs
+++ SoicialMedia/SocialMedia.Infrastructure/Services/PostService.cs
@@ -1,4 +1,8 @@
-﻿using SocialMedia.Application.Common.Dto.Post;
+﻿using System.Net;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Application.Common.Constants.Post;
+using SocialMedia.Application.Common.Dto.Common;
+using SocialMedia.Application.Common.Dto.Post;
 using SocialMedia.Application.Common.Exceptions;
 using SocialMedia.Application.Common.interfaces;
 using SocialMedia.Application.Common.Mappers.Post;
@@ -17,5 +21,33 @@ public class PostService(ISmDbContext dbContext, IUserService userService) : IPo
         await dbContext.SaveChangesAsync(cancellationToken: cancellationToken);
 
         return entity.ToDetailsDto();
+    }
+
+    public async Task<PostDetailsDto?> GetAsync(string id)
+    {
+        var post = await dbContext.Posts.Where(x => x.Id.Equals(Guid.Parse(id))).FirstOrDefaultAsync() ?? throw new NotFoundException("Post not found");
+        return post.ToDetailsDto();
+    }
+
+    public async Task<List<PostDetailsDto>> GetAllAsync()
+    {
+        var posts = await dbContext.Posts.ToListAsync();
+        return posts.ToDetailsListDto();
+    }
+
+    public async Task<PostDetailsDto?> UpdateAsync(UpdatePostDetailsDto dto, CancellationToken cancellationToken)
+    {
+        var post = await dbContext.Posts.Where(x => x.Id.Equals(Guid.Parse(dto.PostId))).FirstOrDefaultAsync(cancellationToken: cancellationToken) ?? throw new NotFoundException("Post not found");
+        post.ToEntity(dto);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return post.ToDetailsDto();
+    }
+
+    public async Task<DeleteResponseDto> DeleteAsync(string id, CancellationToken cancellationToken)
+    {
+        var post = await dbContext.Posts.Where(x => x.Id.Equals(Guid.Parse(id))).FirstOrDefaultAsync(cancellationToken: cancellationToken) ?? throw new NotFoundException("Post not found");
+        dbContext.Posts.Remove(post);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return new DeleteResponseDto(true, HttpStatusCode.OK, PostConstants.PostDeleted, DateTime.Now);
     }
 }
