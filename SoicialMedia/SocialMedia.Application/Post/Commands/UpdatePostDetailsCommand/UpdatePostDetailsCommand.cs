d38e7e04aadd2257aa97e718e64e8f6bff697f69
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-22 19:28:24 +0200

    Created UpdatePostDetailsCommand

---
+++ SoicialMedia/SocialMedia.Application/Post/Commands/UpdatePostDetailsCommand/UpdatePostDetailsCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Post;
+
+namespace SocialMedia.Application.Post.Commands.UpdatePostDetailsCommand;
+
+public record UpdatePostDetailsCommand(UpdatePostDetailsDto Post)  : IRequest<PostDetailsDto?>;
---
+++ SoicialMedia/SocialMedia.Application/Post/Commands/UpdatePostDetailsCommand/UpdatePostDetailsCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Post;
+using SocialMedia.Application.Common.interfaces;
+
+namespace SocialMedia.Application.Post.Commands.UpdatePostDetailsCommand;
+
+public class UpdatePostDetailsCommandHandler(IPostService postService) : IRequestHandler<UpdatePostDetailsCommand, PostDetailsDto?>
+{
+    public async Task<PostDetailsDto?> Handle(UpdatePostDetailsCommand request, CancellationToken cancellationToken)
+    {
+        return await postService.UpdateAsync(request.Post, cancellationToken);
+    }
+}
