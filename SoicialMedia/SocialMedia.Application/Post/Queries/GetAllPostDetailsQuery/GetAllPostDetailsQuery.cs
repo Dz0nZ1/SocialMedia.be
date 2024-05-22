17657dce515d5b35e477b57796e8326de153911c
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-22 19:35:48 +0200

    Created GetAllPostDetailsQuery

---
+++ SoicialMedia/SocialMedia.Application/Post/Queries/GetAllPostDetailsQuery/GetAllPostDetailsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Post;
+
+namespace SocialMedia.Application.Post.Queries.GetAllPostDetailsQuery;
+
+public record GetAllPostDetailsQuery() : IRequest<List<PostDetailsDto>>;
---
+++ SoicialMedia/SocialMedia.Application/Post/Queries/GetAllPostDetailsQuery/GetAllPostDetailsQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Post;
+using SocialMedia.Application.Common.interfaces;
+
+namespace SocialMedia.Application.Post.Queries.GetAllPostDetailsQuery;
+
+public class GetAllPostDetailsQueryHandler(IPostService postService) : IRequestHandler<GetAllPostDetailsQuery, List<PostDetailsDto>>
+{
+    public async Task<List<PostDetailsDto>> Handle(GetAllPostDetailsQuery request, CancellationToken cancellationToken)
+    {
+        return await postService.GetAllAsync();
+    }
+}
