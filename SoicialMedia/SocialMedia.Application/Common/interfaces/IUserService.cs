a0ed9bcede72b50cdcc27db31465f85609eba1f5
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-22 19:38:54 +0200

    Added Crud method definitions

--- SoicialMedia/SocialMedia.Application/Common/interfaces/IUserService.cs
+++ SoicialMedia/SocialMedia.Application/Common/interfaces/IUserService.cs
@@ -1,11 +1,23 @@
-﻿using SocialMedia.Domain.Entities.User;
+﻿using SocialMedia.Application.Common.Dto.Common;
+using SocialMedia.Application.Common.Dto.User;
+using SocialMedia.Domain.Entities.User;
 
 namespace SocialMedia.Application.Common.interfaces;
 
 public interface IUserService
 {
-    Task CreateUserAsync(string emailAddress, List<string> roles);
-    Task<ApplicationUser?> GetUserAsync(string userId);
-    Task<ApplicationUser?> GetUserByEmailAsync(string emailAddress);
+    Task<UserDetailsDto?> CreateUserAsync(CreateUserDetailsDto user, List<string> roles);
+    Task<UserDetailsDto?> GetUserAsync(string userId);
+
+    Task<ApplicationUser?> GetUserEntityAsync(string userId);
+    Task<UserDetailsDto?> GetUserByEmailAsync(string emailAddress);
     Task<bool> IsInRoleAsync(ApplicationUser user, string roleName);
+
+    Task<DeleteResponseDto> DeleteUserAsync(string userId);
+
+    Task<UserDetailsDto?> UpdateUserAsync(UpdateUserDetailsDto dto);
+
+    Task<List<UserDetailsDto>> GetAllUsers();
+
+
 }
