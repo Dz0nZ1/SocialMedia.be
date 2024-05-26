227d19b1cd4de444f51b318b2261bbf211511d0a
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-26 11:35:28 +0200

    Added Crud definitions in IPostService

--- SoicialMedia/SocialMedia.Application/Common/interfaces/IPostService.cs
+++ SoicialMedia/SocialMedia.Application/Common/interfaces/IPostService.cs
@@ -1,8 +1,17 @@
-﻿using SocialMedia.Application.Common.Dto.Post;
+﻿using SocialMedia.Application.Common.Dto.Common;
+using SocialMedia.Application.Common.Dto.Post;
 
 namespace SocialMedia.Application.Common.interfaces;
 
 public interface IPostService
 {
     Task<PostDetailsDto> CreateAsync(CreatePostDetailsDto post, CancellationToken cancellationToken);
+
+    Task<PostDetailsDto?> GetAsync(string id);
+
+    Task<List<PostDetailsDto>> GetAllAsync();
+
+    Task<PostDetailsDto?> UpdateAsync(UpdatePostDetailsDto dto, CancellationToken cancellationToken);
+
+    Task<DeleteResponseDto> DeleteAsync(string id, CancellationToken cancellationToken);
 }
