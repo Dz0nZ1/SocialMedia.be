0ae95cfb3573a9d04a86528d19bde2f710771792
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-28 22:35:04 +0200

    Created CommentDetailsDto and CommentInfoDto

---
+++ SoicialMedia/SocialMedia.Application/Common/Dto/Comment/CommentDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using SocialMedia.Application.Common.Dto.Post;
+using SocialMedia.Application.Common.Dto.User;
+
+namespace SocialMedia.Application.Common.Dto.Comment;
+
+public record CommentDetailsDto(string Content, DateTime CreatedAt, DateTime ModifiedAt, CommentInfo Info)
+{
+    internal CommentDetailsDto AddCommentInfo(CommentInfo commentInfo)
+    {
+        return this with { Info = commentInfo };
+    }
+}
---
+++ SoicialMedia/SocialMedia.Application/Common/Dto/Comment/CommentInfo.cs
@@ -0,0 +1,6 @@
+﻿using SocialMedia.Application.Common.Dto.Post;
+using SocialMedia.Application.Common.Dto.User;
+
+namespace SocialMedia.Application.Common.Dto.Comment;
+
+public record CommentInfo(UserFullNameDto User);
