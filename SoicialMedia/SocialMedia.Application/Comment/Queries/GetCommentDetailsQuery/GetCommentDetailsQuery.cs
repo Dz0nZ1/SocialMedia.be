a283e85a7bb4aaac02097cf93435a03dc9d70c8e
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-29 22:09:04 +0200

    Created GetCommentDetailsQuery

---
+++ SoicialMedia/SocialMedia.Application/Comment/Queries/GetCommentDetailsQuery/GetCommentDetailsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Comment;
+
+namespace SocialMedia.Application.Comment.Queries.GetCommentDetailsCommand;
+
+public record GetCommentDetailsQuery(string Id) : IRequest<CommentDetailsDto?>;
---
+++ SoicialMedia/SocialMedia.Application/Comment/Queries/GetCommentDetailsQuery/GetCommentDetailsQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Comment;
+using SocialMedia.Application.Common.interfaces;
+
+namespace SocialMedia.Application.Comment.Queries.GetCommentDetailsCommand;
+
+public class GetCommentDetailsQueryHandler(ICommentService commentService) : IRequestHandler<GetCommentDetailsQuery, CommentDetailsDto?>
+{
+    public async Task<CommentDetailsDto?> Handle(GetCommentDetailsQuery request, CancellationToken cancellationToken)
+    {
+        return await commentService.GetAsync(request.Id);
+    }
+}
