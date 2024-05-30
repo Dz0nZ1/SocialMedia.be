d5f4f3067e83aa65a8b98d4da0da1f5e04896ae4
Dz0nZ1 <dzonzi777@gmail.com>
2024-05-30 22:31:24 +0200

    Created DeleteCommentDetailsCommand

---
+++ SoicialMedia/SocialMedia.Application/Comment/Commands/DeleteCommentDetilasCommand/DeleteCommentDetailsCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Common;
+
+namespace SocialMedia.Application.Comment.Commands.DeleteCommentDetilasCommand;
+
+public record DeleteCommentDetailsCommand(string Id) : IRequest<DeleteResponseDto>;
---
+++ SoicialMedia/SocialMedia.Application/Comment/Commands/DeleteCommentDetilasCommand/DeleteCommentDetailsCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Common;
+using SocialMedia.Application.Common.interfaces;
+
+namespace SocialMedia.Application.Comment.Commands.DeleteCommentDetilasCommand;
+
+public class DeleteCommentDetailsCommandHandler(ICommentService commentService) : IRequestHandler<DeleteCommentDetailsCommand, DeleteResponseDto>
+{
+    public async Task<DeleteResponseDto> Handle(DeleteCommentDetailsCommand request, CancellationToken cancellationToken)
+    {
+        return await commentService.DeleteAsync(request.Id, cancellationToken);
+    }
+}
