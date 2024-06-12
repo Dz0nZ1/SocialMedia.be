d5ba13156bc591c894c70f6a787f60707290221f
Dz0nZ1 <dzonzi777@gmail.com>
2024-06-12 22:43:06 +0200

    Created post controller

---
+++ SoicialMedia/SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Application.Common.Dto.Common;
+using SocialMedia.Application.Common.Dto.Post;
+using SocialMedia.Application.Post.Commands.CreatePostDetailsCommand;
+using SocialMedia.Application.Post.Commands.DeletePostDetailsCommand;
+using SocialMedia.Application.Post.Commands.UpdatePostDetailsCommand;
+using SocialMedia.Application.Post.Queries.GetAllPostDetailsQuery;
+using SocialMedia.Application.Post.Queries.GetPostDetailsQuery;
+
+namespace SocialMedia.Api.Controllers;
+
+public class PostController : ApiControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<List<PostDetailsDto>>> GetAllPosts([FromQuery] GetAllPostDetailsQuery query) =>
+        Ok(await Mediator.Send(query));
+
+    [HttpGet]
+    public async Task<ActionResult<PostDetailsDto>> GetPost([FromQuery] GetPostDetailsQuery query) =>
+        Ok(await Mediator.Send(query));
+    
+
+    [HttpPost]
+    public async Task<ActionResult<PostDetailsDto>> CreatePost(CreatePostDetailsCommand command) =>
+        Ok(await Mediator.Send(command));
+
+    [HttpPut]
+    public async Task<ActionResult<PostDetailsDto?>> UpdatePost(UpdatePostDetailsCommand command) =>
+        Ok(await Mediator.Send(command));
+
+    [HttpDelete]
+    public async Task<ActionResult<DeleteResponseDto>> DeletePost([FromQuery] DeletePostDetailsCommand command) =>
+        Ok(await Mediator.Send(command));
+
+}
