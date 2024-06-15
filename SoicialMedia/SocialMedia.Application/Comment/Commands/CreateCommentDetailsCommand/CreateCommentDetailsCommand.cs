4714d0aed8c9666dc388de8c0839e0f833c796f0
Dz0nZ1 <dzonzi777@gmail.com>
2024-06-15 13:24:34 +0200

    Created CreateCommentDetailsCommand

---
+++ SoicialMedia/SocialMedia.Application/Comment/Commands/CreateCommentDetailsCommand/CreateCommentDetailsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Comment;
+
+namespace SocialMedia.Application.Comment.Commands.CreateCommentDetailsCommand;
+
+public record CreateCommentDetailsCommand(CreateCommentDetailsDto Comment) : IRequest<CommentDetailsDto>
+{
+    
+}
---
+++ SoicialMedia/SocialMedia.Application/Comment/Commands/CreateCommentDetailsCommand/CreateCommentDetailsCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Comment;
+using SocialMedia.Application.Common.interfaces;
+
+namespace SocialMedia.Application.Comment.Commands.CreateCommentDetailsCommand;
+
+public class CreateCommentDetailsCommandHandler(ICommentService commentService) : IRequestHandler<CreateCommentDetailsCommand, CommentDetailsDto>
+{
+    public async Task<CommentDetailsDto> Handle(CreateCommentDetailsCommand request, CancellationToken cancellationToken)
+    {
+        return await commentService.CreateAsync(request.Comment, cancellationToken);
+    }
+}
