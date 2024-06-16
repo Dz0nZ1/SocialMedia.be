be936dae8df466a67efbb2c32cfd3c698cfd4ab1
Dz0nZ1 <dzonzi777@gmail.com>
2024-06-16 11:30:21 +0200

    Created UpdateCommentDetailsCommand

---
+++ SoicialMedia/SocialMedia.Application/Comment/Commands/UpdateCommentDetailsCommand/UpdateCommentDetailsCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Comment;
+
+namespace SocialMedia.Application.Comment.Commands.UpdateCommentDetailsCommand;
+
+public record UpdateCommentDetailsCommand(UpdateCommentDetailsDto Comment) : IRequest<CommentDetailsDto?>;
---
+++ SoicialMedia/SocialMedia.Application/Comment/Commands/UpdateCommentDetailsCommand/UpdateCommentDetailsCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Comment;
+using SocialMedia.Application.Common.interfaces;
+
+namespace SocialMedia.Application.Comment.Commands.UpdateCommentDetailsCommand;
+
+public class UpdateCommentDetailsCommandHandler(ICommentService commentService) : IRequestHandler<UpdateCommentDetailsCommand, CommentDetailsDto?>
+{
+    public async Task<CommentDetailsDto?> Handle(UpdateCommentDetailsCommand request, CancellationToken cancellationToken)
+    {
+        return await commentService.UpdateAsync(request.Comment, cancellationToken);
+    }
+}
