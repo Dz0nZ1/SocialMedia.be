da0ad4b8eb0da789701d05c5a81be6ae80a19e7f
Dz0nZ1 <dzonzi777@gmail.com>
2024-06-18 20:01:46 +0200

    Created LikeMapper

---
+++ SoicialMedia/SocialMedia.Application/Common/Mappers/Like/LikeMapper.cs
@@ -0,0 +1,19 @@
+﻿using Riok.Mapperly.Abstractions;
+using SocialMedia.Application.Common.Dto.Like;
+using SocialMedia.Application.Common.Dto.Post;
+using SocialMedia.Application.Common.Dto.User;
+using SocialMedia.Application.Common.Mappers.Comment;
+using SocialMedia.Application.Common.Mappers.Post;
+
+namespace SocialMedia.Application.Common.Mappers.Like;
+
+[Mapper]
+public static partial class LikeMapper
+{
+    public static LikeDetailsDto ToDetailsDto(this Domain.Entities.Like entity)
+    {
+        return new LikeDetailsDto(entity.Post!.ToDetailsDto(), entity.Comment!.ToDetailsDto(),
+            new UserFullNameDto(entity.User.FirstName, entity.User.LastName, entity.User.Username), entity.IsLocked);
+    }
+    
+}
