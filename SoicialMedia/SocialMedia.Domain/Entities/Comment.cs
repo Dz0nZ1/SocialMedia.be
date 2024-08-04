6a00242661bf25390b265abc70b60a4f82939898
Dz0nZ1 <dzonzi777@gmail.com>
2024-08-04 16:03:43 +0200

    Updated Comment entity

--- SoicialMedia/SocialMedia.Domain/Entities/Comment.cs
+++ SoicialMedia/SocialMedia.Domain/Entities/Comment.cs
@@ -4,21 +4,79 @@ namespace SocialMedia.Domain.Entities;
 
 public class Comment
 {
-    public Guid Id { get; set; }
+    #region Properties
+
+    public Guid Id { get; private set; }
     
-    public Guid PostId { get; set; }
+    public Guid PostId { get; private set; }
     
-    public Post Post { get; set; }
+    public Post Post { get; private set; }
     
-    public string? UserId { get; set; }
+    public string? UserId { get; private set; }
     
-    public ApplicationUser? User { get; set; }
+    public ApplicationUser? User { get; private set; }
     
-    public string Content { get; set; }
+    public string Content { get; private set; }
     
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; private set; }
     
-    public DateTime ModifiedAt { get; set; }
+    public DateTime ModifiedAt { get; private set; }
 
     public List<Like> Likes = new List<Like>();
+
+    #endregion
+
+    #region Constructors
+
+    public Comment(string content, string userId, Guid postId)
+    {
+        Content = content;
+        UserId = userId;
+        PostId = postId;
+        Id = Guid.NewGuid();
+       
+    }
+    
+    public Comment(string content)
+    {
+        Content = content;
+    }
+
+    #endregion
+    
+
+    #region Extensions
+
+    public Comment AddUser(ApplicationUser user)
+    {
+        User = user;
+        return this;
+    }
+
+    public Comment AddPost(Post post)
+    {
+        Post = post;
+        return this;
+    }
+
+    public Comment AddCreatedAt(DateTime time)
+    {
+        CreatedAt = time;
+        return this;
+    }
+
+    public Comment AddModifiedAt(DateTime time)
+    {
+        ModifiedAt = time;
+        return this;
+    }
+
+    public Comment UpdateComment(string content)
+    {
+        Content = content;
+        ModifiedAt = DateTime.Now;
+        return this;
+    }
+
+    #endregion
 }
