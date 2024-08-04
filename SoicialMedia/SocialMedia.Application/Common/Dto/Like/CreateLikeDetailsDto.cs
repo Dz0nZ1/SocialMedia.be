865bea728f360556ffa4c8fffd42071e4d33dbac
Dz0nZ1 <dzonzi777@gmail.com>
2024-08-04 16:02:07 +0200

    Created more Dtos for Like entity

---
+++ SoicialMedia/SocialMedia.Application/Common/Dto/Like/CreateLikeDetailsDto.cs
@@ -0,0 +1,3 @@
+﻿namespace SocialMedia.Application.Common.Dto.Like;
+
+public record CreateLikeDetailsDto(DateTime CreatedAt, string? PostId, string? CommentId, string UserId);
---
+++ SoicialMedia/SocialMedia.Application/Like/Commands/CreateLikeDetailsCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SocialMedia.Application.Common.Dto.Like;
+
+namespace SocialMedia.Application.Like.Commands;
+
+public record CreateLikeDetailsCommand(CreateLikeDetailsDto Like) : IRequest<LikeDetailsDto>;
