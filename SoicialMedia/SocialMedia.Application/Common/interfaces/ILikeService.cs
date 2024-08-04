bb822f0edcfa23ce8eec3a93486c659f585240c3
Dz0nZ1 <dzonzi777@gmail.com>
2024-08-04 15:58:42 +0200

    Created LikeService

---
+++ SoicialMedia/SocialMedia.Application/Common/interfaces/ILikeService.cs
@@ -0,0 +1,12 @@
+﻿using SocialMedia.Application.Common.Dto.Like;
+
+namespace SocialMedia.Application.Common.interfaces;
+
+public interface ILikeService
+{
+    Task<List<LikeDetailsDto>> GetAllAsync();
+
+    Task<LikeDetailsDto?> GetAsync(string id);
+
+    Task<LikeDetailsDto> CreateAsync();
+}
---
+++ SoicialMedia/SocialMedia.Infrastructure/Services/LikeService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Application.Common.Dto.Like;
+using SocialMedia.Application.Common.Exceptions;
+using SocialMedia.Application.Common.interfaces;
+using SocialMedia.Application.Common.Mappers.Like;
+
+namespace SocialMedia.Infrastructure.Services;
+
+public class LikeService(ISmDbContext dbContext) : ILikeService
+{
+    public async Task<List<LikeDetailsDto>> GetAllAsync()
+    {
+        var likes = await dbContext.Likes.ToListAsync();
+        return likes.ToDetailsListDto();
+    }
+
+    public async Task<LikeDetailsDto?> GetAsync(string id)
+    {
+        var like = await dbContext.Likes.Where(x => x.Id.Equals(Guid.Parse(id))).FirstOrDefaultAsync() ?? throw new NotFoundException("Like not found");
+        return like.ToDetailsDto();
+    }
+
+    public Task<LikeDetailsDto> CreateAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
