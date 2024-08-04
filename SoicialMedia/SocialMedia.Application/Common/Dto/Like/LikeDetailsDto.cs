a4e1f3378371c05562c93ed356dfe577b224d95a
Dz0nZ1 <dzonzi777@gmail.com>
2024-08-04 15:59:10 +0200

    Created LikeDetailsDto

---
+++ SoicialMedia/SocialMedia.Application/Common/Dto/Like/LikeDetailsDto.cs
@@ -0,0 +1,7 @@
+﻿using SocialMedia.Application.Common.Dto.Comment;
+using SocialMedia.Application.Common.Dto.Post;
+using SocialMedia.Application.Common.Dto.User;
+
+namespace SocialMedia.Application.Common.Dto.Like;
+
+public record LikeDetailsDto(PostDetailsDto? Post, CommentDetailsDto? Comment, UserFullNameDto UserFullName, bool isLocked);
